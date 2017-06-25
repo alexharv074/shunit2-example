ba1032479a8352ca55ef24362f7b3d81392c4bf9
Alex Harvey <alexharv074@gmail.com>
2017-06-26 02:39:32 +1000

    Add a third passing test
    Adds a test for a small CIDR range. This also demonstrates how to stub
    out a system command and substitute fake responses.

--- examples/test_prips.sh
+++ examples/test_prips.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# Fake the output of the ipcalc.
+ipcalc() {
+  case "$*" in
+  "-n 192.168.0.2/28")
+    echo NETWORK=192.168.0.0
+    ;;
+  "-b 192.168.0.2/28")
+    echo BROADCAST=192.168.0.15
+    ;;
+  esac
+}
+
 test_minus_h() {
   first_line=$(. ./prips.sh -h | head -1)
   assertEquals 'Print all IPs in a CIDR range, similar to the Ubuntu prips utility.' "$first_line"
@@ -8,6 +20,14 @@ test_minus_h() {
 test_missing_args() {
   first_line=$(. ./prips.sh | head -1)
   assertEquals 'You must pass a CIDR' "$first_line"
+}
+
+test_a_little_cidr() {
+  response=$(. ./prips.sh 192.168.0.2/28)
+  expected="192.168.0.0 192.168.0.1 192.168.0.2 192.168.0.3 192.168.0.4 \
+192.168.0.5 192.168.0.6 192.168.0.7 192.168.0.8 192.168.0.9 192.168.0.10 \
+192.168.0.11 192.168.0.12 192.168.0.13 192.168.0.14 192.168.0.15"
+  assertEquals "$expected" "$response"
 }
 
 . shunit2
